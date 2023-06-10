2b4c09c5fe55b01cef577294179691a66bd2949a
Limkon <130622161+Limkon@users.noreply.github.com>
2023-06-10 19:44:59 +0800

    Update remove_duplicates.py

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -12,8 +12,15 @@ def remove_duplicates_from_file(filename):
                 lines_seen.add(line)  # 将行添加到已出现的行集合中
                 output_lines.append(line)  # 将行添加到输出列表中
 
-    with open(filename, 'w') as file:
-        file.write('\n'.join(output_lines))  # 将输出列表中的行写入文件，并使用换行符分隔
+    # 将结果写回原文件
+    try:
+        with open(filename, 'w') as file:
+            file.write('\n'.join(output_lines))  # 将输出列表中的行写入文件，并使用换行符分隔
+        print("去重操作完成并已将结果写回原文件。")
+    except IOError:
+        print("无法写入结果到原文件，以下是去重后的结果:")
+        for line in output_lines:
+            print(line)
 
 # 获取命令行参数
 if len(sys.argv) != 2:
