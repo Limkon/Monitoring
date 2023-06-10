46df1d26acc4547d75b190d10c6b2ea8e5a32d43
Limkon <130622161+Limkon@users.noreply.github.com>
2023-06-10 16:58:41 +0800

    Update remove_duplicates.py

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -4,10 +4,16 @@ def remove_duplicates(file_path):
     with open(file_path, 'r') as file:
         lines = file.readlines()
     
+    # 去除每行末尾的换行符
+    lines = [line.strip() for line in lines]
+    
+    # 去重 URL
     lines = list(set(lines))
     
-    with open(file_path, 'w') as file:
-        file.writelines(lines)
+    # 输出去重后的 URL 到新文件
+    new_file_path = "urls_no_duplicates"
+    with open(new_file_path, 'w') as file:
+        file.writelines(line + '\n' for line in lines)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
