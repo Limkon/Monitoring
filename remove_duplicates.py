89264533724f970e06fca7d5d8a4f4430e629885
Limkon <130622161+Limkon@users.noreply.github.com>
2023-06-10 17:11:04 +0800

    Update remove_duplicates.py

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,17 +1,15 @@
 import sys
+import filecmp
+import shutil
 
 def remove_duplicates(file_path):
     with open(file_path, 'r') as file:
         lines = file.readlines()
     
-    # 去除每行末尾的换行符
-    lines = [line.strip() for line in lines]
-    
-    # 去重 URL
     lines = list(set(lines))
     
     with open(file_path, 'w') as file:
-        file.writelines(line + '\n' for line in lines)
+        file.writelines(lines)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
@@ -19,4 +17,18 @@ if __name__ == '__main__':
         sys.exit(1)
     
     file_path = sys.argv[1]
+    temp_file_path = file_path + '_no_duplicates'
+    
+    # Remove duplicates and save to temporary file
     remove_duplicates(file_path)
+    remove_duplicates(temp_file_path)
+    
+    # Compare temporary file with original file
+    if not filecmp.cmp(file_path, temp_file_path):
+        # If different, update the original file
+        shutil.move(temp_file_path, file_path)
+        print("URLs file updated.")
+    else:
+        # If same, remove the temporary file
+        shutil.rmtree(temp_file_path)
+        print("URLs file unchanged.")
