d4ae0af3f8d3fbaaff7774963bcc13479e3a0af3
Limkon <130622161+Limkon@users.noreply.github.com>
2023-06-10 16:35:54 +0800

    Update remove_duplicates.py

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,13 +1,13 @@
-lines_seen = set()  # 用于跟踪已经出现过的行
-output_lines = []  # 用于存储去重后的行
+def remove_duplicates(input_file, output_file):
+    with open(input_file, 'r') as file:
+        lines = file.readlines()
 
-with open('urls', 'r') as file:
-    for line in file:
-        line = line.strip()  # 去除行首尾的空白字符
+    unique_lines = set(lines)
 
-        if line not in lines_seen:  # 如果行不在已出现的行集合中
-            lines_seen.add(line)  # 将行添加到已出现的行集合中
-            output_lines.append(line)  # 将行添加到输出列表中
+    with open(output_file, 'w') as file:
+        file.writelines(unique_lines)
 
-with open('urls', 'w') as file:
-    file.write('\n'.join(output_lines))  # 将输出列表中的行写入文件，并使用换行符分隔
+    print("去除重复行完成！")
+
+# 使用示例
+remove_duplicates('urls', 'urls_no_duplicates')
