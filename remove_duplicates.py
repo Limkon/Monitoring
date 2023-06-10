3c529327ab037cef1d548fae6f4d85baf4e67e73
Limkon <130622161+Limkon@users.noreply.github.com>
2023-06-10 16:49:39 +0800

    Update remove_duplicates.py

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,13 +1,18 @@
-def remove_duplicates(input_file, output_file):
-    with open(input_file, 'r') as file:
+import sys
+
+def remove_duplicates(file_path):
+    with open(file_path, 'r') as file:
         lines = file.readlines()
+    
+    lines = list(set(lines))
+    
+    with open(file_path, 'w') as file:
+        file.writelines(lines)
 
-    unique_lines = set(lines)
-
-    with open(output_file, 'w') as file:
-        file.writelines(unique_lines)
-
-    print("去除重复行完成！")
-
-# 使用示例
-remove_duplicates('urls', 'urls_no_duplicates')
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python remove_duplicates.py <file_path>")
+        sys.exit(1)
+    
+    file_path = sys.argv[1]
+    remove_duplicates(file_path)
