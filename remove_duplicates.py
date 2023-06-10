f58a7ba4f428e21d41b4c226ae76e59e0111c388
Limkon <130622161+Limkon@users.noreply.github.com>
2023-06-10 19:49:17 +0800

    Update remove_duplicates.py

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 def remove_duplicates_from_file(filename):
@@ -12,13 +13,15 @@ def remove_duplicates_from_file(filename):
                 lines_seen.add(line)  # 将行添加到已出现的行集合中
                 output_lines.append(line)  # 将行添加到输出列表中
 
-    # 将结果写回原文件
+    # 将结果写回根目录中的 'urls' 文件
+    root_dir = os.getcwd()
+    output_filename = os.path.join(root_dir, 'urls')
     try:
-        with open(filename, 'w') as file:
+        with open(output_filename, 'w') as file:
             file.write('\n'.join(output_lines))  # 将输出列表中的行写入文件，并使用换行符分隔
-        print("去重操作完成并已将结果写回原文件。")
+        print("去重操作完成并已将结果写回根目录中的 'urls' 文件。")
     except IOError:
-        print("无法写入结果到原文件，以下是去重后的结果:")
+        print("无法写入结果到 'urls' 文件，以下是去重后的结果:")
         for line in output_lines:
             print(line)
 
