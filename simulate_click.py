35fc7db9e7a979b125bb554ba6e0ea2d12098ada
Limkon <130622161+Limkon@users.noreply.github.com>
2023-06-15 20:56:32 +0800

    Update simulate_click.py

--- simulate_click.py
+++ simulate_click.py
@@ -2,11 +2,16 @@ from selenium import webdriver
 import sys
 
 try:
+    if len(sys.argv) < 2:
+        raise ValueError("请提供要访问的 URL 参数")
+
     # 从命令行参数获取要访问的 URL
     url = sys.argv[1]
 
     # 创建浏览器驱动，例如使用 Chrome 浏览器
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')  # 以无头模式运行，可选
+    driver = webdriver.Chrome(options=options)
 
     # 打开网页
     driver.get(url)
@@ -17,8 +22,10 @@ try:
 
     # 关闭浏览器
     driver.quit()
+
 except Exception as e:
-    print(e)
+    print(f"发生错误：{str(e)}")
+
 finally:
     # 不管是否发生异常都会执行的代码
     pass
