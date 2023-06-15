8561dd03e8f40088ab79533b06a0bf8441eb7658
Limkon <130622161+Limkon@users.noreply.github.com>
2023-06-15 20:35:29 +0800

    Update simulate_click.py

--- simulate_click.py
+++ simulate_click.py
@@ -1,19 +1,23 @@
 from selenium import webdriver
-
-# 从命令行参数获取要访问的 URL
 import sys
-url = sys.argv[1]
 
-# 创建浏览器驱动，例如使用 Chrome 浏览器
-driver = webdriver.Chrome()
+try:
+    # 从命令行参数获取要访问的 URL
+    url = sys.argv[1]
 
-# 打开网页
-driver.get(url)
+    # 创建浏览器驱动，例如使用 Chrome 浏览器
+    driver = webdriver.Chrome()
 
-# 执行你想要的点击操作，例如点击一个按钮
-button = driver.find_element_by_id('my-button')
-button.click()
+    # 打开网页
+    driver.get(url)
 
-# 关闭浏览器
-driver.quit()
+    # 执行你想要的点击操作，例如点击一个按钮
+    button = driver.find_element_by_id('my-button')
+    button.click()
 
+    # 关闭浏览器
+    driver.quit()
+except Exception as e:
+    print(e)
+finally:
+    # 不管是否发生异常都会执行的代码
