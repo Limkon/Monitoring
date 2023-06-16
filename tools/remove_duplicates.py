8896429a6ed397bbd181070da0db202ea74311a8
Limkon <130622161+Limkon@users.noreply.github.com>
2023-06-16 09:50:36 +0800

    Update remove_duplicates.py

--- tools/remove_duplicates.py
+++ tools/remove_duplicates.py
@@ -1,11 +1,11 @@
 import os
 import sys
 
-def remove_duplicates_from_file(filename):
+def remove_duplicates_from_file(input_filename, output_filename):
     lines_seen = set()  # 用于跟踪已经出现过的行
     output_lines = []  # 用于存储去重后的行
 
-    with open(filename, 'r') as file:
+    with open(input_filename, 'r') as file:
         for line in file:
             line = line.strip()  # 去除行首尾的空白字符
 
@@ -13,21 +13,19 @@ def remove_duplicates_from_file(filename):
                 lines_seen.add(line)  # 将行添加到已出现的行集合中
                 output_lines.append(line)  # 将行添加到输出列表中
 
-    # 将结果写回根目录中的 'urls' 文件
-    root_dir = os.getcwd()
-    output_filename = os.path.join(root_dir, 'urls')
     try:
         with open(output_filename, 'w') as file:
             file.write('\n'.join(output_lines))  # 将输出列表中的行写入文件，并使用换行符分隔
-        print("去重操作完成并已将结果写回根目录中的 'urls' 文件。")
+        print("去重操作完成并已将结果写入指定的文件。")
     except IOError:
-        print("无法写入结果到 'urls' 文件，以下是去重后的结果:")
+        print("无法写入结果到指定的文件，以下是去重后的结果:")
         for line in output_lines:
             print(line)
 
 # 获取命令行参数
-if len(sys.argv) != 2:
-    print("Usage: python remove_duplicates.py <filename>")
+if len(sys.argv) != 3:
+    print("Usage: python remove_duplicates.py <input_filename> <output_filename>")
 else:
-    filename = sys.argv[1]  # 获取文件名参数
-    remove_duplicates_from_file(filename)  # 调用去重函数，传入文件名参数
+    input_filename = sys.argv[1]  # 获取输入文件名参数
+    output_filename = sys.argv[2]  # 获取输出文件名参数
+    remove_duplicates_from_file(input_filename, output_filename)  # 调用去重函数，传入文件名参数
