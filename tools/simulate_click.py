48ecd8ffc1e5254d0ebf9927bfadfeb4c4080fce
Limkon <130622161+Limkon@users.noreply.github.com>
2023-06-20 23:00:01 +0800

    Update simulate_click.py

--- tools/simulate_click.py
+++ tools/simulate_click.py
@@ -17,9 +17,6 @@ failed_urls = []
 
 for url in urls:
     try:
-        # 模拟点击页面操作
-        subprocess.run(["python", "./tools/simulate_click.py", url], check=True)
-
         # 执行 HTTP 请求，并处理响应
         response = requests.get(url, timeout=5)
         if response.status_code >= 200 and response.status_code < 300:
@@ -30,9 +27,9 @@ for url in urls:
             responses.append(f"{url}: 失败 ({response.status_code})")
             failed_urls.append(f"{url}: 失败 ({response.status_code})")
 
-    except subprocess.CalledProcessError:
-        print(f"{url}: 执行命令失败")
-        responses.append(f"{url}: 执行命令失败")
-        failed_urls.append(f"{url}: 执行命令失败")
+    except requests.RequestException as e:
+        print(f"{url}: 请求失败 ({str(e)})")
+        responses.append(f"{url}: 请求失败 ({str(e)})")
+        failed_urls.append(f"{url}: 请求失败 ({str(e)})")
 
     print("")
