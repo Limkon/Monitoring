f956cdd9e9a33fc70d6c6474adff3a398d2fc759
Limkon <130622161+Limkon@users.noreply.github.com>
2023-06-20 22:52:27 +0800

    Update simulate_click.py

--- tools/simulate_click.py
+++ tools/simulate_click.py
@@ -1,5 +1,6 @@
 import random
 import subprocess
+import requests
 import sys
 
 # 从命令行参数中获取要访问的 URL 列表文件名
@@ -19,18 +20,15 @@ for url in urls:
         # 模拟点击页面操作
         subprocess.run(["python", "./tools/simulate_click.py", url], check=True)
 
-        # 执行 curl 请求，并处理响应
-        command = ["timeout", "5s", "curl", "-sf", "-o", "/dev/null", "-w", "%{http_code}\\n", url]
-        result = subprocess.run(command, capture_output=True, text=True)
-        response = result.stdout.strip()
-
-        if response.startswith("2") or response.startswith("3"):
+        # 执行 HTTP 请求，并处理响应
+        response = requests.get(url, timeout=5)
+        if response.status_code >= 200 and response.status_code < 300:
             print(f"{url}: 成功")
             responses.append(f"{url}: 成功")
         else:
-            print(f"{url}: 失败 ({response})")
-            responses.append(f"{url}: 失败 ({response})")
-            failed_urls.append(f"{url}: 失败 ({response})")
+            print(f"{url}: 失败 ({response.status_code})")
+            responses.append(f"{url}: 失败 ({response.status_code})")
+            failed_urls.append(f"{url}: 失败 ({response.status_code})")
 
     except subprocess.CalledProcessError:
         print(f"{url}: 执行命令失败")
