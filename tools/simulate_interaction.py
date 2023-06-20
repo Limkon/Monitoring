234e0d680195e1e5e0368cd9324db013860c5416
Limkon <130622161+Limkon@users.noreply.github.com>
2023-06-20 23:18:45 +0800

    Update simulate_interaction.py

--- tools/simulate_interaction.py
+++ tools/simulate_interaction.py
@@ -19,27 +19,40 @@ for url in urls:
     # 去除行末的换行符
     url = url.strip()
 
-    # 打开网页
-    driver.get(url)
+    # 初始化完成标志
+    is_complete = False
 
-    # 随机点击元素
-    elements = driver.find_elements_by_tag_name("a")
-    if elements:
-        random_element = random.choice(elements)
-        random_element.click()
-        print("执行点击操作")
+    # 尝试执行操作，直到完成
+    while not is_complete:
+        try:
+            # 打开网页
+            driver.get(url)
 
-    # 随机移动鼠标
-    actions = ActionChains(driver)
-    actions.move_to_element(random_element).perform()
-    print("执行移动鼠标操作")
+            # 随机点击元素
+            elements = driver.find_elements_by_tag_name("a")
+            if elements:
+                random_element = random.choice(elements)
+                random_element.click()
+                print("执行点击操作")
 
-    # 随机滚动页面
-    scroll_distance = random.randint(200, 500)
-    actions = ActionChains(driver)
-    actions.send_keys(Keys.PAGE_DOWN * scroll_distance).perform()
-    print("执行滚动页面操作")
+            # 随机移动鼠标
+            actions = ActionChains(driver)
+            actions.move_to_element(random_element).perform()
+            print("执行移动鼠标操作")
 
+            # 随机滚动页面
+            scroll_distance = random.randint(200, 500)
+            actions = ActionChains(driver)
+            actions.send_keys(Keys.PAGE_DOWN * scroll_distance).perform()
+            print("执行滚动页面操作")
+
+            is_complete = True  # 操作成功完成
+
+        except Exception as e:
+            # 捕获异常并打印错误信息
+            print(f"操作失败: {e}")
+            print("重新尝试操作...")
+    
     print(f"操作的URL: {url}")
     print("")
 
