c7dcf19e9ec2a4e50a70aff0687adcc24f3d10c4
Limkon <130622161+Limkon@users.noreply.github.com>
2023-06-21 10:25:47 +0800

    Update simulate_browsing.py

--- tools/simulate_browsing.py
+++ tools/simulate_browsing.py
@@ -1,54 +1,69 @@
 import time
 import random
-import argparse
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+import logging
 
-def simulate_operations(url_file):
+# 创建日志记录器
+logging.basicConfig(filename='browsing.log', level=logging.INFO)
+
+def simulate_operations(filename):
     # 创建浏览器实例
     driver = webdriver.Chrome()
 
-    # 读取URL地址文件
-    with open(url_file, 'r') as file:
-        urls = file.readlines()
+    try:
+        # 读取URL文件
+        with open(filename, 'r') as file:
+            urls = file.readlines()
 
-    # 循环处理每个网址
-    for url in urls:
-        url = url.strip()  # 去除换行符和空格
+        # 遍历URL列表
+        for url in urls:
+            url = url.strip()  # 移除换行符和空格
 
-        # 打开网页
-        driver.get(url)
+            try:
+                # 打开网页
+                driver.get(url)
+                logging.info(f"成功打开网页: {url}")
 
-        # 最大化窗口
-        driver.maximize_window()
+                # 最大化窗口
+                driver.maximize_window()
+                logging.info("窗口已最大化")
 
-        # 模拟鼠标点击
-        element = driver.find_element(By.XPATH, "//a[@id='my-link']")
-        ActionChains(driver).move_to_element(element).click().perform()
+                # 模拟操作
+                element = driver.find_element(By.XPATH, "//a[@id='my-link']")
+                ActionChains(driver).move_to_element(element).click().perform()
+                logging.info("模拟鼠标点击成功")
 
-        # 模拟随机移动
-        actions = ActionChains(driver)
-        actions.move_to_element_with_offset(element, 10, 10)
-        for _ in range(5):
-            actions.move_by_offset(random.randint(-50, 50), random.randint(-50, 50))
-            actions.perform()
-            time.sleep(1)
+                # 模拟随机移动
+                actions = ActionChains(driver)
+                actions.move_to_element_with_offset(element, 10, 10)
+                for _ in range(5):
+                    actions.move_by_offset(random.randint(-50, 50), random.randint(-50, 50))
+                    actions.perform()
+                    time.sleep(1)
+                logging.info("模拟随机移动成功")
 
-        # 模拟滚动网页
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(2)
-        driver.execute_script("window.scrollTo(0, 0);")
-        time.sleep(2)
+                # 模拟滚动网页
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                time.sleep(2)
+                driver.execute_script("window.scrollTo(0, 0);")
+                time.sleep(2)
+                logging.info("模拟滚动网页成功")
 
-    # 关闭浏览器
-    driver.quit()
+            except Exception as e:
+                logging.error(f"执行操作时出现异常: {str(e)}")
 
-# 解析命令行参数
-parser = argparse.ArgumentParser(description='Simulate browsing operations')
-parser.add_argument('file', help='URL addresses file name')
-args = parser.parse_args()
+    except FileNotFoundError:
+        logging.error("URL文件未找到")
 
-# 以命令行参数中的文件名调用函数
-filename = args.file
-simulate_operations(filename)
+    finally:
+        # 关闭浏览器
+        driver.quit()
+        logging.info("浏览器已关闭")
+
+# 在主程序中调用 simulate_operations() 方法
+if __name__ == '__main__':
+    import sys
+    filename = sys.argv[1] if len(sys.argv) > 1 else "urls.txt"
+    simulate_operations(filename)
