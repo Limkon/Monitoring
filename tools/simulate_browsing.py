11a68cbf2b8069aadc4d9df6392f1f7f94f71529
Limkon <130622161+Limkon@users.noreply.github.com>
2023-06-21 10:31:26 +0800

    Update simulate_browsing.py

--- tools/simulate_browsing.py
+++ tools/simulate_browsing.py
@@ -3,10 +3,6 @@ import random
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
-import logging
-
-# 创建日志记录器
-logging.basicConfig(filename='browsing.log', level=logging.INFO)
 
 def simulate_operations(filename):
     # 创建浏览器实例
@@ -24,16 +20,16 @@ def simulate_operations(filename):
             try:
                 # 打开网页
                 driver.get(url)
-                logging.info(f"成功打开网页: {url}")
+                print(f"成功打开网页: {url}")
 
                 # 最大化窗口
                 driver.maximize_window()
-                logging.info("窗口已最大化")
+                print("窗口已最大化")
 
                 # 模拟操作
                 element = driver.find_element(By.XPATH, "//a[@id='my-link']")
                 ActionChains(driver).move_to_element(element).click().perform()
-                logging.info("模拟鼠标点击成功")
+                print("模拟鼠标点击成功")
 
                 # 模拟随机移动
                 actions = ActionChains(driver)
@@ -42,25 +38,25 @@ def simulate_operations(filename):
                     actions.move_by_offset(random.randint(-50, 50), random.randint(-50, 50))
                     actions.perform()
                     time.sleep(1)
-                logging.info("模拟随机移动成功")
+                print("模拟随机移动成功")
 
                 # 模拟滚动网页
                 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
                 time.sleep(2)
                 driver.execute_script("window.scrollTo(0, 0);")
                 time.sleep(2)
-                logging.info("模拟滚动网页成功")
+                print("模拟滚动网页成功")
 
             except Exception as e:
-                logging.error(f"执行操作时出现异常: {str(e)}")
+                print(f"执行操作时出现异常: {str(e)}")
 
     except FileNotFoundError:
-        logging.error("URL文件未找到")
+        print("URL文件未找到")
 
     finally:
         # 关闭浏览器
         driver.quit()
-        logging.info("浏览器已关闭")
+        print("浏览器已关闭")
 
 # 在主程序中调用 simulate_operations() 方法
 if __name__ == '__main__':
