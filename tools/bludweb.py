afa87d1023b406f1e9bcbf6b83a1ff9bcc8f9e2e
Limkon <130622161+Limkon@users.noreply.github.com>
2023-07-05 14:17:16 +0800

    Update bludweb.py

--- tools/bludweb.py
+++ tools/bludweb.py
@@ -24,12 +24,24 @@ def generate_code_file(directory):
         file.write(code)
     print(f"生成文件：{filename}.{code_type}")
 
-# 通过命令行参数获取目录
-if len(sys.argv) < 2:
-    print("请提供目录作为命令行参数")
+def count_files_in_directory(directory):
+    # 统计目录中的文件数
+    file_count = sum(1 for _ in os.scandir(directory) if _.is_file())
+    return file_count
+
+def clear_directory(directory):
+    # 清空目录中的文件
+    for file in os.scandir(directory):
+        if file.is_file():
+            os.remove(file.path)
+
+# 通过命令行参数获取目录和文件数阈值
+if len(sys.argv) < 3:
+    print("请提供目录和文件数阈值作为命令行参数")
     sys.exit(1)
 
 target_directory = sys.argv[1]
+threshold = int(sys.argv[2])
 
 # 如果目录不存在，则创建目录
 if not os.path.exists(target_directory):
@@ -38,3 +50,11 @@ if not os.path.exists(target_directory):
 
 # 生成代码文件
 generate_code_file(target_directory)
+
+# 统计文件数并清空目录
+num_files = count_files_in_directory(target_directory)
+print(f"当前文件数：{num_files}")
+
+if num_files > threshold:
+    clear_directory(target_directory)
+    print("目录已清空")
