475bfd702550332b1495ea84c11a043daac825f4
Limkon <130622161+Limkon@users.noreply.github.com>
2023-07-05 14:24:06 +0800

    Update bludweb.py

--- tools/bludweb.py
+++ tools/bludweb.py
@@ -2,38 +2,29 @@ import os
 import random
 import string
 import sys
+from jinja2 import Template
 
-def generate_random_code():
-    # 生成随机的代码
-    code_type = random.choice(["html", "css", "js"])
-    code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
-    if code_type == "html":
-        return f"<html><body>{code}</body></html>"
-    elif code_type == "css":
-        return f"body {{ color: {code}; }}"
-    elif code_type == "js":
-        return f"console.log('{code}');"
+def generate_random_data():
+    # 随机生成一些数据，用于在代码模板中使用
+    return {
+        'name': ''.join(random.choices(string.ascii_uppercase, k=5)),
+        'color': random.choice(['red', 'green', 'blue']),
+        'number': random.randint(1, 10)
+    }
 
 def generate_code_file(directory):
     # 在指定目录中随机生成代码文件
-    code = generate_random_code()
     code_type = random.choice(["html", "css", "js"])
-    filename = ''.join(random.choices(string.ascii_lowercase + string.digits, k=10))
-    filepath = os.path.join(directory, f"{filename}.{code_type}")
-    with open(filepath, 'w') as file:
-        file.write(code)
-    print(f"生成文件：{filename}.{code_type}")
-
-def count_files_in_directory(directory):
-    # 统计目录中的文件数
-    file_count = sum(1 for _ in os.scandir(directory) if _.is_file())
-    return file_count
-
-def clear_directory(directory):
-    # 清空目录中的文件
-    for file in os.scandir(directory):
-        if file.is_file():
-            os.remove(file.path)
+    template_file = f"templates/{code_type}.jinja2"
+    output_file = f"{directory}/code.{code_type}"
+    with open(template_file, 'r') as file:
+        template_content = file.read()
+        template = Template(template_content)
+        data = generate_random_data()
+        code = template.render(data=data)
+        with open(output_file, 'w') as output:
+            output.write(code)
+    print(f"生成文件：code.{code_type}")
 
 # 通过命令行参数获取目录和文件数阈值
 if len(sys.argv) < 3:
