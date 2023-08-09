8d3a0d8f1e000ebb1ec16a4fe37fa9d1b430d5d9
Limkon <130622161+Limkon@users.noreply.github.com>
2023-08-09 19:43:48 +0800

    Update bludweb.py

--- tools/bludweb.py
+++ tools/bludweb.py
@@ -27,21 +27,19 @@ def generate_random_filename():
         filename = ''.join(random.choices(string.ascii_letters + string.digits, k=random.randint(8, 12)))
     return filename
 
-def generate_code_file(directory):
-    # 在指定目录中随机生成代码文件
-    code_types = ["html", "css", "js"]
-    for code_type in code_types:
-        template_file = f"templates/{code_type}.jinja2"
-        output_file = f"{directory}/{generate_random_filename()}.{code_type}"
-        os.makedirs(os.path.dirname(output_file), exist_ok=True)  # 创建目录
-        with open(template_file, 'r') as file:
-            template_content = file.read()
-            template = Template(template_content)
-            data = generate_random_data()  # 生成随机数据
-            code = template.render(data=data)  # 使用随机数据渲染模板
-            with open(output_file, 'w') as output:
-                output.write(code)
-                print(f"生成文件：{os.path.basename(output_file)}")  # 添加这行以显示生成的文件名
+def generate_code_file(directory, code_type):
+    # 在指定目录中随机生成指定类型的代码文件
+    template_file = f"templates/{code_type}.jinja2"
+    output_file = f"{directory}/{generate_random_filename()}.{code_type}"
+    os.makedirs(os.path.dirname(output_file), exist_ok=True)  # 创建目录
+    with open(template_file, 'r') as file:
+        template_content = file.read()
+        template = Template(template_content)
+        data = generate_random_data()  # 生成随机数据
+        code = template.render(data=data)  # 使用随机数据渲染模板
+        with open(output_file, 'w') as output:
+            output.write(code)
+            print(f"生成文件：{os.path.basename(output_file)}")  # 添加这行以显示生成的文件名
 
 def remove_directory_contents(directory):
     # 递归删除目录及其内容
@@ -66,8 +64,9 @@ if not os.path.exists(target_directory):
     os.makedirs(target_directory)
     print(f"目录 {target_directory} 创建成功")
 
-# 生成代码文件
-generate_code_file(target_directory)
+# 随机选择一个类型（js、html 或 css）来生成代码文件
+code_type = random.choice(["js", "html", "css"])
+generate_code_file(target_directory, code_type)
 
 # 统计文件数并清空目录及其内容
 num_files = count_files_in_directory(target_directory)
