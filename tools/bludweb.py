52c4c8dba6a749736f1029a5ab0c665494b7e86f
Limkon <130622161+Limkon@users.noreply.github.com>
2023-08-09 19:23:06 +0800

    Update bludweb.py

--- tools/bludweb.py
+++ tools/bludweb.py
@@ -5,10 +5,12 @@ import sys
 from jinja2 import Template
 
 def count_files_in_directory(directory):
+    # 统计目录中的文件数
     file_count = sum(1 for _ in os.scandir(directory) if _.is_file())
     return file_count
 
 def generate_random_data():
+    # 随机生成一些数据，用于在代码模板中使用
     return {
         'title': ''.join(random.choices(string.ascii_uppercase, k=10)),
         'heading': 'Random Heading',
@@ -17,27 +19,34 @@ def generate_random_data():
     }
 
 def generate_random_filename():
+    # 生成随机文件名，可以是包含字母、数字和特殊字符的组合，也可以是纯字母或纯数字
     characters = string.ascii_letters + string.digits + string.punctuation
     filename = ''.join(random.choices(characters, k=random.randint(8, 12)))
     if random.choice([True, False]):
+        # 纯字母或纯数字
         filename = ''.join(random.choices(string.ascii_letters + string.digits, k=random.randint(8, 12)))
     return filename
 
-def generate_code_file(directory):
+def generate_code_file(directory, num_files=1):  # 添加 num_files 参数
+    # 在指定目录中随机生成代码文件
     code_type = random.choice(["html", "css", "js"])
     template_file = f"templates/{code_type}.jinja2"
-    output_file = f"{directory}/{generate_random_filename()}.{code_type}"
-    os.makedirs(os.path.dirname(output_file), exist_ok=True)
-    with open(template_file, 'r') as file:
-        template_content = file.read()
-        template = Template(template_content)
-        data = generate_random_data()
-        code = template.render(data=data)
-        with open(output_file, 'w') as output:
-            output.write(code)
-            print(f"Generated file: {os.path.basename(output_file)}")
+    
+    os.makedirs(directory, exist_ok=True)  # 创建目录
+    
+    for _ in range(num_files):  # 循环生成多个文件
+        output_file = f"{directory}/{generate_random_filename()}.{code_type}"
+        with open(template_file, 'r') as file:
+            template_content = file.read()
+            template = Template(template_content)
+            data = generate_random_data()  # 生成随机数据
+            code = template.render(data=data)  # 使用随机数据渲染模板
+            with open(output_file, 'w') as output:
+                output.write(code)
+                print(f"生成文件：{os.path.basename(output_file)}")  # 添加这行以显示生成的文件名
 
 def remove_directory_contents(directory):
+    # 递归删除目录及其内容
     for root, dirs, files in os.walk(directory, topdown=False):
         for file in files:
             file_path = os.path.join(root, file)
@@ -46,23 +55,27 @@ def remove_directory_contents(directory):
             dir_path = os.path.join(root, dir)
             os.rmdir(dir_path)
 
-if len(sys.argv) < 3:
-    print("Please provide the directory and file count threshold as command line arguments")
+# 通过命令行参数获取目录和文件数阈值
+if len(sys.argv) < 4:  # 修改为 < 4
+    print("请提供目录、文件数阈值和生成文件数作为命令行参数")  # 修改提示信息
     sys.exit(1)
 
 target_directory = sys.argv[1]
 threshold = int(sys.argv[2])
+num_to_generate = int(sys.argv[3])  # 获取生成文件数
 
+# 如果目录不存在，则创建目录
 if not os.path.exists(target_directory):
     os.makedirs(target_directory)
-    print(f"Directory {target_directory} created successfully")
+    print(f"目录 {target_directory} 创建成功")
 
-for _ in range(threshold):
-    generate_code_file(target_directory)
+# 生成代码文件
+generate_code_file(target_directory, num_to_generate)  # 使用 num_to_generate 参数
 
+# 统计文件数并清空目录及其内容
 num_files = count_files_in_directory(target_directory)
-print(f"Current file count: {num_files}")
+print(f"当前文件数：{num_files}")
 
 if num_files > threshold:
     remove_directory_contents(target_directory)
-    print("Directory cleared")
+    print("目录已清空")
