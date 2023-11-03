e8e756ffc4a7adbf386cbcdfa15eeeb529ff6e05
Limkon <130622161+Limkon@users.noreply.github.com>
2023-11-03 22:03:21 +0800

    Update _worker.js

--- work_cf/_worker.js
+++ work_cf/_worker.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '0058c4cc-82a2-4cd0-92ed-fe8286d261d2';
 
 const proxyIPs = ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
 
