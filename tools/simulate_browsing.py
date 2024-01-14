f66d504609fadab1dd9428b6de5d8c53e12ba355
Limkon <130622161+Limkon@users.noreply.github.com>
2024-01-14 21:50:46 +0800

    Update simulate_browsing.py

--- tools/simulate_browsing.py
+++ tools/simulate_browsing.py
@@ -1,11 +1,3 @@
-import time
-import random
-import concurrent.futures
-from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.common.exceptions import NoSuchElementException
-import os
-
 def process_url(url):
     # 创建浏览器实例
     driver = webdriver.Chrome()
@@ -60,6 +52,9 @@ def process_url(url):
         print("刷新网页")
         time.sleep(2)
 
+        # 维持网页打开一段时间
+        time.sleep(15)
+
     except Exception as e:
         print(f"执行操作时出现异常: {str(e)}")
 
@@ -71,32 +66,3 @@ def process_url(url):
         # 设置输出值为成功打开的网页URL
         output_name = f"URL_{url}"
         os.environ[output_name] = url
-
-def simulate_operations(filename):
-    try:
-        # 读取URL文件
-        with open(filename, 'r') as file:
-            urls = file.readlines()
-
-        # 创建线程池，最大线程数为10（可根据实际情况调整）
-        with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
-            # 提交每个URL的处理任务到线程池
-            futures = [executor.submit(process_url, url.strip()) for url in urls]
-
-            # 等待所有任务完成
-            concurrent.futures.wait(futures)
-
-        # 保存输出到环境文件
-        with open("outputs.env", "w") as env_file:
-            for url in urls:
-                output_name = f"URL_{url.strip()}"
-                env_file.write(f"{output_name}={os.environ.get(output_name, '')}\n")
-
-    except FileNotFoundError:
-        print("URL文件未找到")
-
-# 在主程序中调用 simulate_operations() 方法
-if __name__ == '__main__':
-    import sys
-    filename = sys.argv[1] if len(sys.argv) > 1 else "urls.txt"
-    simulate_operations(filename)
