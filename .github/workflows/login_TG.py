817cf04ca16883fb19f0f34d7906ad9cd72c2b4e
Limkon <130622161+Limkon@users.noreply.github.com>
2024-02-15 19:45:06 +0800

    Update login_TG.py

--- .github/workflows/login_TG.py
+++ .github/workflows/login_TG.py
@@ -27,15 +27,5 @@ jobs:
         run: |
           pip install requests
 
-      - name: Send Telegram Message
-        run: |
-          TELEGRAM_BOT_TOKEN=$YOUR_TELEGRAM_BOT_TOKEN
-          CHAT_ID=$YOUR_CHAT_ID
-          MESSAGE="Hello from GitHub Actions on branch ${{ github.event.inputs.branch }}!"
-
-          curl -s -X POST https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage \
-            -d chat_id=$CHAT_ID \
-            -d text="$MESSAGE"
-        env:
-          YOUR_TELEGRAM_BOT_TOKEN: ${{ secrets.TELEGRAM_BOT_TOKEN }}
-          YOUR_CHAT_ID: ${{ secrets.TELEGRAM_CHAT_ID }}
+      - name: Build web
+        run: python ./tools/webtelegram.py
