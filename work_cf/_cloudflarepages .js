4402904758bd93df701be88b2098462ef811067c
Limkon <wo@fr.ht>
2024-08-17 12:29:15 +0800

    Update _cloudflarepages .js

--- work_cf/_cloudflarepages .js
+++ work_cf/_cloudflarepages .js
@@ -2,24 +2,20 @@ export default {
     async fetch(request, env) {
         let url = new URL(request.url);
 
-        // 检查路径是否符合你的自定义逻辑（如检查路径或其他条件）
         if (url.pathname.startsWith('/')) {
-            // 通过拼接的方式设置主机名（url.hostname 是只读的）
-            let newUrl = `${request.url.replace(url.hostname, env.HOSTNAME || "m3u8-player.com")}`;
+            // 使用 PROXY_HOST 环境变量
+            url.hostname = env.PROXY_HOST || "m3u8-player.com";
 
-            // 创建新请求，保留原始请求的所有属性
-            let newRequest = new Request(newUrl, {
+            let newRequest = new Request(url.toString(), {
                 method: request.method,
                 headers: request.headers,
                 body: request.body,
                 redirect: request.redirect
             });
 
-            // 发送更新后的请求
             return fetch(newRequest);
         }
 
-        // 如果路径不符合条件，则从 ASSETS 中获取请求
         return env.ASSETS.fetch(request);
     }
 };
