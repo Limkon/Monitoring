28cbae16242042d074772e6ca3eed46288d62dc2
Limkon <wo@fr.ht>
2024-08-18 16:20:28 +0800

    Update simulate_browsing.py

--- tools/simulate_browsing.py
+++ tools/simulate_browsing.py
@@ -1,28 +1,28 @@
+import time
+import random
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException
+import os
+
 def process_url(url):
-    # 创建浏览器实例
     driver = webdriver.Chrome()
 
     try:
-        # 打开网页
         driver.get(url)
         print(f"成功打开网页: {url}")
-
-        # 最大化窗口
         driver.maximize_window()
         print("窗口已最大化")
 
-        window_size = driver.get_window_size()  # 获取窗口大小
+        window_size = driver.get_window_size()
 
-        # 随机选择一种操作并执行5次
         for _ in range(5):
-            operation = random.randint(1, 3)  # 1: 模拟点击, 2: 模拟移动鼠标, 3: 模拟滚动网页
+            operation = random.randint(1, 3)
 
             if operation == 1:  # 模拟点击
                 try:
-                    # 随机生成点击位置的坐标
-                    x = random.randint(0, window_size['width'])
-                    y = random.randint(0, window_size['height'])
-                    # 执行点击操作
+                    x = random.randint(0, window_size['width'] - 1)
+                    y = random.randint(0, window_size['height'] - 1)
                     element = driver.find_element("tag name", "body")
                     ActionChains(driver).move_to_element_with_offset(element, x, y).click().perform()
                     print(f"模拟点击成功：坐标({x}, {y})")
@@ -42,27 +42,22 @@ def process_url(url):
                 actions.perform()
 
             else:  # 模拟滚动网页
-                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);"
-                                      "window.scrollTo(0, 0);")
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                driver.execute_script("window.scrollTo(0, 0);")
                 print("模拟滚动到页面底部和顶部")
                 time.sleep(2)
 
-        # 刷新网页
         driver.refresh()
         print("刷新网页")
         time.sleep(2)
-
-        # 维持网页打开一段时间
         time.sleep(15)
 
     except Exception as e:
         print(f"执行操作时出现异常: {str(e)}")
 
     finally:
-        # 关闭浏览器
         driver.quit()
         print("浏览器已关闭")
 
-        # 设置输出值为成功打开的网页URL
         output_name = f"URL_{url}"
         os.environ[output_name] = url
