c347f2e8179e9fe1ad39b6ffe209f573a725dbb4
Limkon <wo@fr.ht>
2024-09-21 21:36:23 +0800

    Update brow.sh

--- tools/brow.sh
+++ tools/brow.sh
@@ -14,17 +14,14 @@ while IFS= read -r url; do
     if [ ! -z "$url" ]; then
         echo "正在访问 $url ..."
         
-        # 启动 Chrome，并将输出重定向到日志文件
+        # 使用 timeout 包装 Chrome 命令
         {
-            xvfb-run --server-args="-screen 0 1280x1024x24" google-chrome --headless --no-sandbox --disable-gpu --disable-dev-shm-usage --remote-debugging-port=9222 --timeout=30 "$url"
-        } &> chrome_log.txt
-        
-        # 检查 Chrome 是否成功启动
-        if [ $? -ne 0 ]; then
-            echo "访问 $url 失败，请检查 chrome_log.txt 以获取更多信息。"
-        else
+            timeout 30 xvfb-run --server-args="-screen 0 1280x1024x24" google-chrome --headless --no-sandbox --disable-gpu --disable-dev-shm-usage --remote-debugging-port=9222 "$url" &> chrome_log.txt
+        } && {
             echo "访问 $url 成功！"
-        fi
+        } || {
+            echo "访问 $url 失败或超时！查看 chrome_log.txt 以获取更多信息。"
+        }
     fi
 done < "url.txt"
 
