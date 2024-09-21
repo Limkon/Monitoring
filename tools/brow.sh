792fed84cd6e75e6c4e6a41929463d21aa1b19db
Limkon <wo@fr.ht>
2024-09-21 21:28:14 +0800

    Update brow.sh

--- tools/brow.sh
+++ tools/brow.sh
@@ -13,7 +13,18 @@ export DBUS_SESSION_BUS_ADDRESS=/dev/null
 while IFS= read -r url; do
     if [ ! -z "$url" ]; then
         echo "正在访问 $url ..."
-        xvfb-run -a google-chrome --headless --no-sandbox --disable-gpu --disable-dev-shm-usage --remote-debugging-port=9222 "$url"
+        
+        # 启动 Chrome，并将输出重定向到日志文件
+        {
+            xvfb-run --server-args="-screen 0 1280x1024x24" google-chrome --headless --no-sandbox --disable-gpu --disable-dev-shm-usage --remote-debugging-port=9222 --timeout=30 "$url"
+        } &> chrome_log.txt
+        
+        # 检查 Chrome 是否成功启动
+        if [ $? -ne 0 ]; then
+            echo "访问 $url 失败，请检查 chrome_log.txt 以获取更多信息。"
+        else
+            echo "访问 $url 成功！"
+        fi
     fi
 done < "url.txt"
 
