f6ea33aafb06afb7d67f2f1bad2e79f88abedaad
Limkon <wo@fr.ht>
2024-10-19 22:00:08 +0800

    Update BPB_worker.js

--- work_cf/BPB_worker.js
+++ work_cf/BPB_worker.js
@@ -2,6 +2,7 @@
 // <!--GAMFC-->version base on commit 43fad05dcdae3b723c53c226f8181fc5bd47223e, time is 2023-06-22 15:20:02 UTC<!--GAMFC-END-->.
 // @ts-ignore
 // https://github.com/amclubs/BPB-Worker-Panel
+// KV bpb
 
 import { connect } from 'cloudflare:sockets';
 
