10a699ea5699b7a8788863f7c78bcd65e2d1af43
Limkon <wo@fr.ht>
2024-11-27 22:34:05 +0800

    Update Vless_workers_pages2024.11.25.js

--- work_cf/Vless_workers_pages2024.11.25.js
+++ work_cf/Vless_workers_pages2024.11.25.js
@@ -4,7 +4,7 @@ import { connect } from "cloudflare:sockets";
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = "86c50e3a-5b87-49dd-bd20-03c7f2735e40";
+let userID = "0058c4cc-82a2-4cd0-92ed-fe8286d261d2";
 
 const proxyIPs = ["ts.hpc.tw"]; //ts.hpc.tw edgetunnel.anycast.eu.org bestproxy.onecf.eu.org cdn-all.xn--b6gac.eu.org cdn.xn--b6gac.eu.org proxy.xxxxxxxx.tk
 const cn_hostnames = [''];
