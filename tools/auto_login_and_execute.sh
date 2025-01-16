ec4b4df4941539ea1020b045a4dbafccb889429e
Limkon <wo@fr.ht>
2025-01-16 22:12:38 +0800

    Update auto_login_and_execute.sh

--- tools/auto_login_and_execute.sh
+++ tools/auto_login_and_execute.sh
@@ -9,7 +9,7 @@ execute_commands() {
     crontab -l > x-ui.cron
 
     # 删除包含$enable_str的行
-    sed -i "" "/$enable_str/d" x-ui.cron
+    sed -i "/$enable_str/d" x-ui.cron
 
     # 添加新的@reboot任务
     echo "@reboot cd $cur_dir/x-ui && nohup ./x-ui run > ./x-ui.log 2>&1 &" >> x-ui.cron
@@ -21,22 +21,11 @@ execute_commands() {
     rm x-ui.cron
 }
 
-# 无限循环
-while true; do
-  # 尝试使用SSH密钥登录服务器并执行命令
-  ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -tt womimi@panel15.serv00.com << EOF
+# 使用sshpass登录并执行命令
+sshpass -p '19821124Rl' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -tt womimi@panel15.serv00.com << EOF
     $(typeset -f execute_commands)
     execute_commands
     exit
 EOF
-  
-  # 检查SSH命令的退出状态码
-  if [ $? -eq 0 ]; then
-    echo "命令执行成功，退出SSH会话"
-  else
-    echo "无法登录或命令执行失败"
-  fi
 
-  # 休眠30天（259200秒）
-  sleep 259200
-done
+echo "命令执行完成，已退出SSH会话"
