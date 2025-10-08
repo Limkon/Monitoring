1d2ae4c5f94b3ebc442ed45b8095e6107ac437e2
Limkon <wo@fr.ht>
2025-10-08 08:57:44 +0800

    Delete webs directory

--- webs/TQu92qyYdb.py
+++
@@ -1,9 +0,0 @@
-# Title_yxo1Kb
-import random
-
-def main():
-    print("Heading_yxo1Kb")
-    print("Content generated at 00:50:57: yxo1Kb")
-
-if __name__ == "__main__":
-    main()
--- webs/sozSWrrlPJ.py
+++
@@ -1,9 +0,0 @@
-# Title_PMs7Ks
-import random
-
-def main():
-    print("Heading_PMs7Ks")
-    print("Content generated at 00:39:40: PMs7Ks")
-
-if __name__ == "__main__":
-    main()
